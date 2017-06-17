5a80be37cc52f090725ade647d70f31a7dd17f35
Raskolnikov <millionsvetil@gmail.com>
2017-06-17 14:22:10 +0300

    add project files

---
+++ musicDirectory/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="musicDirectory.MvcApplication" Language="C#" %>
---
+++ musicDirectory/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(musicDirectory.Startup))]
+namespace musicDirectory
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
